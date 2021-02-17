dade5f3d0353d04c261d2159b1fb81ab07893540
Cassio Favaretto <cassio.favaretto@compasso.com.br>
2021-02-17 10:44:43 -0300

    scope

--- src/main/java/com/project/hangman/controller/HangmanController.java
+++ src/main/java/com/project/hangman/controller/HangmanController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.annotation.SessionScope;
 
 import com.project.hangman.domain.Hangman;
 import com.project.hangman.exception.GameNotStartedException;
@@ -13,6 +15,7 @@ import com.project.hangman.exception.InvalidInputException;
 import com.project.hangman.service.HangmanService;
 
 @Controller
+@SessionScope
 public class HangmanController {
 
 	@Autowired
@@ -28,5 +31,11 @@ public class HangmanController {
 	public ResponseEntity<Hangman> playGame(@PathVariable String letter) throws GameNotStartedException, InvalidInputException {
 		Hangman play = hangmanService.play(letter);
 		return ResponseEntity.status(HttpStatus.OK).body(play);
+	}
+
+	@GetMapping("/session/message")
+	public ResponseEntity<String> getMessage() {
+		System.out.println(this);
+		return ResponseEntity.status(HttpStatus.OK).body("Hello");
 	}
 }
